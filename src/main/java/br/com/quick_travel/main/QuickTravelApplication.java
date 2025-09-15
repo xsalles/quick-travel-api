6d54fef762a4f0574641d10f0f3f4197ba11f73c
xsalles <xs.salles@gmail.com>
2025-09-15 16:10:31 -0300

    feat: started the repository with my back end application

---
+++ src/main/java/br/com/quick_travel/main/QuickTravelApplication.java
@@ -0,0 +1,13 @@
+package br.com.quick_travel.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/quick_travel/main/QuickTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quick_travel.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
