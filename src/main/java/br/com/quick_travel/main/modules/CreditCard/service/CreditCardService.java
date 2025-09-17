3dc00aafe2c17704fe50aab686dafceb774cfa5d
xsalles <xs.salles@gmail.com>
2025-09-16 22:24:28 -0300

    feat: now I can't create the same credit card more than once

--- src/main/java/br/com/quick_travel/main/modules/CreditCard/service/CreditCardService.java
+++ src/main/java/br/com/quick_travel/main/modules/CreditCard/service/CreditCardService.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.quick_travel.main.common.dto.ApiResponseDto;
+import br.com.quick_travel.main.common.exceptions.EntityAlreadyExistsException;
 import br.com.quick_travel.main.modules.CreditCard.model.CreditCardModel;
 import br.com.quick_travel.main.modules.CreditCard.repository.CreditCardRepository;
 import br.com.quick_travel.main.modules.User.repository.UserRepository;
@@ -26,20 +27,27 @@ public class CreditCardService {
     public ResponseEntity<ApiResponseDto<String>> addCreditCardForUser(HttpServletRequest request,
             CreditCardModel creditCardModel) {
         var userId = request.getAttribute("user_id");
-        
+
         if (userId == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-            .body(new ApiResponseDto<String>("Sorry, you can't add a credit card, make login again please.",
+                    .body(new ApiResponseDto<String>("Sorry, you can't add a credit card, make login again please.",
                             HttpStatus.UNAUTHORIZED.value(), null));
         }
-        
+
         var user = userRepository.findById(UUID.fromString(userId.toString()));
-        
+
         if (user.isEmpty()) {
             throw new EntityNotFoundException("User not found.");
         }
 
         List<CreditCardModel> creditCards = user.get().getCreditCards();
+
+        boolean alreadyExists = creditCards.stream()
+                .anyMatch(card -> card.getNumberCard().equals(creditCardModel.getNumberCard()));
+
+        if (alreadyExists) {
+            throw new EntityAlreadyExistsException("This credit card already exists for this user.");
+        }
 
         creditCardModel.setUser(user.get());
 
