b6aa5ba22286b18886e1d3f757335e056502a4b1
xsalles <xs.salles@gmail.com>
2025-09-17 22:16:58 -0300

    feat: added two exceptions to solve the problems that I was getting just forbidden and not the response

--- src/main/java/br/com/quick_travel/main/common/handlers/EntityNotValidInformation.java
+++ src/main/java/br/com/quick_travel/main/common/handlers/EntityNotValidInformation.java
@@ -1,5 +1,26 @@
 package br.com.quick_travel.main.common.handlers;
 
-public class EntityNotValidInformation extends Run {
-    ct
+import br.com.quick_travel.main.common.dto.ApiResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class EntityNotValidInformation {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiResponseDto<String>> handleEntityNotValidInformation(ConstraintViolationException ex, HttpServletRequest request) {
+        var path = request.getRequestURI();
+
+        if (path.startsWith("/credit-cards/add")) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                    .body(new ApiResponseDto<String>("Invalid number card, please try again", HttpStatus.UNPROCESSABLE_ENTITY.value(), null));
+        }
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body(new ApiResponseDto<String>("Invalid CEP, please try again", HttpStatus.UNPROCESSABLE_ENTITY.value(), null));
+    }
 }
